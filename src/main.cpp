9953a3048c0ae39b2eb3939aab0c1505c8f32637
Vladimir Lopatinski <vlopatin@c3r2p6.hive.fi>
2025-09-01 16:38:46 +0300

    add: Server class to manage server

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
-#include <iostream>
+#include "webserv.hpp"
+#include "server/Server.hpp"
+
+int server_fd;
+
+void	handle_sigint(int sig) {
+	(void)sig;
+	close(server_fd);
+	printf("\nServer closed\n");
+	exit(0);
+}
 
 int main()
 {
+	signal(SIGINT, handle_sigint);
+
+	Server server;
+
+	int server_fd = server.create();
+
+	while (true)
+	{
+		int client_fd = accept(server_fd, nullptr, nullptr);
+		if (client_fd < 0)
+		{
+			perror("accept");
+			continue ;
+		}
+
+		std::cout << "Client connected\n" << std::endl;
+
+		char buffer[1024];
+		int bytes;
+		while ((bytes = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
+			// Echo back what was received
+			send(client_fd, buffer, bytes, 0);
+		}
+		close(client_fd);
+		std::cout << "Client disconnected\n";
+	}
+	close(server_fd);
 	return 0;
 }
---
+++ src/server/Server.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "webserv.hpp"
+
+class Server {
+
+	private:
+
+	public:
+		int create();
+};
