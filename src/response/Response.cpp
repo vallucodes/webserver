53015d380d578d298eff3d61c2e1b720bb9f7c4c
Ilia Munaev <ilyamunaev@gmail.com>
2025-09-02 21:19:33 +0300

    Add: default values, methods

--- src/response/Response.cpp
+++ src/response/Response.cpp
@@ -1,7 +1,23 @@
 #include "Response.hpp"
 
+// setup default values
 Response::Response() {
-
+  _status = "200 OK"; // ? what is the default status ?
+  _headers = "";
+  _body = "";
 }
 
 Response::~Response() {}
+
+void Response::setStatus(std::string status) {
+  _status = status;
+}
+
+void Response::setHeaders(std::string headers) {
+  _headers = headers;
+}
+
+// should be template
+void Response::setBody(std::string body) {
+  _body = body;
+}
--- src/response/Response.hpp
+++ src/response/Response.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
 class Response {
+  public:
+    Response();
+    ~Response();
 
-	private:
+    void setStatus(std::string status);
+    void setHeaders(std::string headers);
+    void setBody(std::string body);
 
-	public:
-		Response();
-		~Response();
+  private:
+    std::string _status;
+    std::string _headers;
+    std::string _body;
 };
