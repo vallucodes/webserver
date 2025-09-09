8e4b3033d6123f6ff04a0796076e19482a4037a2
Ilia Munaev <ilyamunaev@gmail.com>
2025-09-09 18:37:53 +0300

    Fix: response header

--- src/response/Response.hpp
+++ src/response/Response.hpp
@@ -2,8 +2,8 @@
 
 #include <string>
 #include <iostream>
+#include <map>
 #include "../message/AMessage.hpp"
-#include "../request/Request.hpp"
 
 class Response: public AMessage {
   public:
@@ -14,6 +14,12 @@ class Response: public AMessage {
 
     std::string_view getStatus() const;
     void setStatus(const std::string& status);
+
+    // Header management
+    void setHeader(const std::string& key, const std::string& value);
+    std::string getHeader(const std::string& key) const;
+    std::string getAllHeaders() const;
+
   private:
     std::string _status;
     std::map<std::string, std::string> _headers;
